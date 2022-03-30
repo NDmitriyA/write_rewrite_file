a592f3afdb272cc237cf1c78baf37556a8a00a8d
Дмитрий Никифоров <nikiforov_d.1973@mail.ru>
2022-03-30 11:30:06 +0400

    MY final

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
 import os
 from pprint import pprint
-#
-#
-# def dict_collector():
-#     file_path = os.path.join(os.getcwd(), 'recipies.txt')
-#     cook_book = {}
-#     with open(file_path, encoding='utf-8') as file_work:
-#         for line in file_work:
-#             dish_name = line.strip()
-#             counter = int(file_work.readline().strip())
-#             list_of_ingredient = []
-#             for i in range(counter):
-#                 dish_items = {}
-#                 ingredient = file_work.readline().strip().split(' | ')
-#                 dish_items['ingredient_name'], dish_items['quantity'], dish_items['measure'] = ingredient
-#                 list_of_ingredient.append(dish_items)
-#             file_work.readline()
-#             cook_book[dish_name] = list_of_ingredient
-#         return cook_book
-#
-#
-# def get_shop_list_by_dishes(dishes, pers_count):
-#     ingr_list = dict()
-#
-#     for dish_name in dishes:
-#         if dish_name in cook_book:
-#             for ings in cook_book[dish_name]:
-#                 order_list = dict()
-#                 if ings['ingredient_name'] not in ingr_list:
-#                     order_list['measure'] = ings['measure']
-#                     order_list['quantity'] = int(ings['quantity']) * pers_count
-#                     ingr_list[ings['ingredient_name']] = order_list
-#                 else:
-#                     ingr_list[ings['ingredient_name']]['quantity'] = ingr_list[ings['ingredient_name']]['quantity'] + \
-#                                                                      int(ings['quantity']) * pers_count
-#         else:
-#             print(f'\n"Такого блюда нет в списке!"\n')
-#     return ingr_list
+
+
+def dict_collector():
+    file_path = os.path.join(os.getcwd(), 'recipies.txt')
+    cook_book = {}
+    with open(file_path, encoding='utf-8') as file_work:
+        for line in file_work:
+            dish_name = line.strip()
+            counter = int(file_work.readline().strip())
+            list_of_ingredient = []
+            for i in range(counter):
+                dish_items = {}
+                ingredient = file_work.readline().strip().split(' | ')
+                dish_items['ingredient_name'], dish_items['quantity'], dish_items['measure'] = ingredient
+                list_of_ingredient.append(dish_items)
+            file_work.readline()
+            cook_book[dish_name] = list_of_ingredient
+        return cook_book
+
+
+def get_shop_list_by_dishes(dishes, pers_count):
+    ingr_list = dict()
+
+    for dish_name in dishes:
+        if dish_name in cook_book:
+            for ings in cook_book[dish_name]:
+                order_list = dict()
+                if ings['ingredient_name'] not in ingr_list:
+                    order_list['measure'] = ings['measure']
+                    order_list['quantity'] = int(ings['quantity']) * pers_count
+                    ingr_list[ings['ingredient_name']] = order_list
+                else:
+                    ingr_list[ings['ingredient_name']]['quantity'] = ingr_list[ings['ingredient_name']]['quantity'] + \
+                                                                     int(ings['quantity']) * pers_count
+        else:
+            print(f'\n"Такого блюда нет в списке!"\n')
+    return ingr_list
 
 
 def rewrite_file(path1=None, path2=None, path3=None):
@@ -56,25 +56,63 @@ def rewrite_file(path1=None, path2=None, path3=None):
             file3 = fp3.readlines()
         with open(final_file, 'w', encoding='utf-8') as f_f:
             if len(file3) > len(file1) < len(file2) < len(file3):
-                f_f.write(path1, '\n' + str(len(file1)), '\n')
+                f_f.write(path1 + '\n' + str(len(file1)) + '\n')
                 f_f.writelines(file1)
-                f_f.write('\n', path2, '\n' + str(len(file2)), '\n')
+                f_f.write('\n')
+                f_f.write('\n' + path2 + '\n' + str(len(file2)) + '\n')
                 f_f.writelines(file2)
-                f_f.write('\n', path3, '\n' + str(len(file3)), '\n')
+                f_f.write('\n')
+                f_f.write('\n' + path3 + '\n' + str(len(file3)) + '\n')
                 f_f.writelines(file3)
+            elif len(file2) > len(file1) < len(file3) < len(file2):
+                f_f.write(path1 + '\n' + str(len(file1)) + '\n')
+                f_f.writelines(file1)
+                f_f.write('\n')
+                f_f.write('\n' + path3 + '\n' + str(len(file3)) + '\n')
+                f_f.writelines(file3)
+                f_f.write('\n')
+                f_f.write('\n' + path2 + '\n' + str(len(file2)) + '\n')
+                f_f.writelines(file2)
             elif len(file1) > len(file2) < len(file3) < len(file1):
-                f_f.write(path2, '\n' + str(len(file2)), '\n')
+                f_f.write(path2 + '\n' + str(len(file2)) + '\n')
                 f_f.writelines(file2)
-                f_f.write('\n', path3, '\n' + str(len(file3)), '\n')
+                f_f.write('\n')
+                f_f.write('\n' + path3 + '\n' + str(len(file3)) + '\n')
                 f_f.writelines(file3)
-                f_f.write('\n', path1, '\n' + str(len(file1)), '\n')
+                f_f.write('\n')
+                f_f.write('\n' + path1 + '\n' + str(len(file1)) + '\n')
                 f_f.writelines(file1)
+            elif len(file1) > len(file2) < len(file3) > len(file1):
+                f_f.write(path2 + '\n' + str(len(file2)) + '\n')
+                f_f.writelines(file2)
+                f_f.write('\n')
+                f_f.write('\n' + path1 + '\n' + str(len(file1)) + '\n')
+                f_f.writelines(file1)
+                f_f.write('\n')
+                f_f.write('\n' + path3 + '\n' + str(len(file3)) + '\n')
+                f_f.writelines(file3)
             elif len(file2) > len(file3) < len(file1) < len(file2):
+                f_f.write(path3 + '\n' + str(len(file3)) + '\n')
+                f_f.writelines(file3)
+                f_f.write('\n')
+                f_f.write('\n' + path1 + '\n' + str(len(file1)) + '\n')
+                f_f.writelines(file1)
+                f_f.write('\n')
+                f_f.write('\n' + path2 + '\n' + str(len(file2)) + '\n')
+                f_f.writelines(file2)
+            else:
+                f_f.write(path3 + '\n' + str(len(file3)) + '\n')
+                f_f.writelines(file3)
+                f_f.write('\n')
+                f_f.write('\n' + path2 + '\n' + str(len(file2)) + '\n')
+                f_f.writelines(file2)
+                f_f.write('\n')
+                f_f.write('\n' + path1 + '\n' + str(len(file1)) + '\n')
+                f_f.writelines(file1)
 
 
-
-# cook_book = dict_collector()
-# pprint(dict_collector())
-# print(('--------------'))
-# pprint(get_shop_list_by_dishes(dishes=['Омлет', 'Омлет'], pers_count=2))
+cook_book = dict_collector()
+pprint(dict_collector())
+print(('--------------'))
+pprint(get_shop_list_by_dishes(dishes=['Омлет', 'Омлет'], pers_count=2))
 rewrite_file()
